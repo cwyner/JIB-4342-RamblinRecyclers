f7edd4aecf8742f1ed6bcc048d050f8bfbaba95d
edwardjin1 <edwardjin2935@gmail.com>
2025-03-08 17:37:52 -0500

    Added items and ability to edit items in Materials

--- app/(app)/(tabs)/(materials)/index.tsx
+++ app/(app)/(tabs)/(materials)/index.tsx
@@ -1,8 +1,13 @@
-import { withTheme } from "react-native-paper"
-import { View } from "react-native"
+import { withTheme } from "react-native-paper";
+import { View } from "react-native";
+import EditDonations from "@/components/ui/EditDonations"; // Ensure this path is correct
 
 function Materials({ theme }: { theme: any }) {
-  return <View></View>
+  return (
+    <View>
+      <EditDonations />
+    </View>
+  );
 }
 
-export default withTheme(Materials)
+export default withTheme(Materials);
